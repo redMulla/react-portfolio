f74cb0eb466da766c0d940ee26c46a006b046234
redMulla <ivassillycharite@gmail.com>
2024-07-14 22:44:43 +0200

    ratings testimonial

--- src/components/CardTestimonials.tsx
+++ src/components/CardTestimonials.tsx
@@ -2,6 +2,9 @@ import { Box, Heading, Image, Text, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 import 'react-loading-skeleton/dist/skeleton.css';
 import Skeleton from 'react-loading-skeleton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
 
 export const CardTestimonials = () => {
   
@@ -12,8 +15,15 @@ export const CardTestimonials = () => {
           {/* <Skeleton height={'60px'} width={'100%'} count={6}/> */}
           <Image src='https://images.pexels.com/photos/6618822/pexels-photo-6618822.jpeg'/>
         </Box>
-        <Heading margin={'5'} fontSize={'md'} fontWeight={'500'} >Ibinkwiye Vassilly delivered a fantastic full-stack web service for us. They were responsive, efficient, and the final product is excellent. Highly recommend!"</Heading>
-        <Text justifySelf={'center'} textColor={'GrayText'}>- John Doe</Text>
+        <Heading margin={'5'}  fontSize={'md'} fontWeight={'500'} >Ibinkwiye Vassilly delivered a fantastic full-stack web service for us. They were responsive, efficient, and the final product is excellent. Highly recommend!"</Heading>
+        <Text justifySelf={'center'} textColor={'GrayText'} mb={'3'}>- John Doe</Text>
+        <Box position={'absolute'} bottom={'-13px'} bg={'var(--chakra-colors-chakra-body-bg)'} px={'2'} borderRadius={'10px'} border={'1px'}>
+          <FontAwesomeIcon icon={faStarSolid} />
+          <FontAwesomeIcon icon={faStarSolid} />
+          <FontAwesomeIcon icon={faStarSolid} />
+          <FontAwesomeIcon icon={faStarRegular} />
+          <FontAwesomeIcon icon={faStarRegular} />
+        </Box>
     </Box>
   )
 }
