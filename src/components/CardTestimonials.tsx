4ecc0704d199dbb1c6a45c0c98835dec42acd941
redMulla <ivassillycharite@gmail.com>
2024-07-18 16:47:24 +0200

    edit

--- src/components/CardTestimonials.tsx
+++ src/components/CardTestimonials.tsx
@@ -1,5 +1,5 @@
-import { Box, Heading, Image, Text, useColorModeValue } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Heading, Image, Text, useColorModeValue } from '@chakra-ui/react';
+import React from 'react';
 import 'react-loading-skeleton/dist/skeleton.css';
 import Skeleton from 'react-loading-skeleton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,23 +7,57 @@ import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons';
 
 export const CardTestimonials = () => {
-  
   return (
-    <Box w="sm"  bg={useColorModeValue('gray.100', 'blue.900')} border={'2px solid'} borderColor={'cyan'} position={'relative'} borderRadius={'15px'} display={'flex'}flexDir={'column'} justifyContent={"center"} alignItems={'center'} paddingTop={'3'} paddingBottom={'3'}>
-        {/* Avatar */}
-        <Box height={'80px'} minH={'0px'} width={'80px'} minW={"60px"} borderRadius={'50%'}  overflow={'hidden'} margin={'3'} border={'1px solid'}>
-          {/* <Skeleton height={'60px'} width={'100%'} count={6}/> */}
-          <Image src='https://images.pexels.com/photos/6618822/pexels-photo-6618822.jpeg'/>
-        </Box>
-        <Heading margin={'5'}  fontSize={'md'} fontWeight={'500'} >Ibinkwiye Vassilly delivered a fantastic full-stack web service for us. They were responsive, efficient, and the final product is excellent. Highly recommend!"</Heading>
-        <Text justifySelf={'center'} textColor={'GrayText'} mb={'3'}>- John Doe</Text>
-        <Box position={'absolute'} bottom={'-13px'} bg={'var(--chakra-colors-chakra-body-bg)'} px={'2'} borderRadius={'10px'} border={'2px'}>
-          <FontAwesomeIcon icon={faStarSolid} />
-          <FontAwesomeIcon icon={faStarSolid} />
-          <FontAwesomeIcon icon={faStarSolid} />
-          <FontAwesomeIcon icon={faStarRegular} />
-          <FontAwesomeIcon icon={faStarRegular} />
-        </Box>
+    <Box
+      w="sm"
+      bg={useColorModeValue('gray.100', 'black')}
+      border={'4px solid'}
+      position={'relative'}
+      borderRadius={'15px'}
+      borderColor={'green'}
+      display={'flex'}
+      flexDir={'column'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      paddingTop={'3'}
+      paddingBottom={'3'}
+    >
+      {/* Avatar */}
+      <Box
+        height={'80px'}
+        minH={'0px'}
+        width={'80px'}
+        minW={'60px'}
+        borderRadius={'50%'}
+        overflow={'hidden'}
+        margin={'3'}
+        border={'1px solid'}
+      >
+        {/* <Skeleton height={'60px'} width={'100%'} count={6}/> */}
+        <Image src="https://images.pexels.com/photos/6618822/pexels-photo-6618822.jpeg" />
+      </Box>
+      <Heading margin={'5'} fontSize={'md'} fontWeight={'500'}>
+        Ibinkwiye Vassilly delivered a fantastic full-stack web service for us.
+        They were responsive, efficient, and the final product is excellent.
+        Highly recommend!"
+      </Heading>
+      <Text justifySelf={'center'} textColor={'GrayText'} mb={'3'}>
+        - John Doe
+      </Text>
+      <Box
+        position={'absolute'}
+        bottom={'-13px'}
+        bg={'var(--chakra-colors-chakra-body-bg)'}
+        px={'2'}
+        borderRadius={'10px'}
+        border={'2px'}
+      >
+        <FontAwesomeIcon icon={faStarSolid} />
+        <FontAwesomeIcon icon={faStarSolid} />
+        <FontAwesomeIcon icon={faStarSolid} />
+        <FontAwesomeIcon icon={faStarRegular} />
+        <FontAwesomeIcon icon={faStarRegular} />
+      </Box>
     </Box>
-  )
-}
+  );
+};
