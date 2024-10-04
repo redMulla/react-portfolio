5ca256096fa3f47f4b8c234ba4181cf20fbb6200
redMulla <ivassillycharite@gmail.com>
2024-10-04 12:32:08 +0200

    project section initialization

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import { Container, Heading, Text } from '@chakra-ui/react';
+import { Box, Container, Flex, Heading, Image } from '@chakra-ui/react';
 import React from 'react';
 
 export const Projects = () => {
@@ -6,17 +6,47 @@ export const Projects = () => {
     <Container
       maxW={'container.2xl'}
       w={'100%'}
+      overflow={'hidden'}
       display={'flex'}
+      flexDir={'column'}
       justifyContent={'center'}
       alignItems={'center'}
       py={'20'}
       //   bg={'var(--chakra-colors-chakra-body-bg)'}
       bg={'rgba(0, 0, 0, 0.8)'}
     >
-      <Heading textColor={'white'} fontSize={'6xl'}>
+      <Heading textColor={'white'} fontSize={'5xl'}>
         Projects
       </Heading>
-      ;
+      <Flex
+        mt={'90px'}
+        gap={20}
+        flexWrap={'wrap'}
+        justifyContent={'center'}
+        alignItems={'center'}
+      >
+        <Box
+          boxSize={300}
+          border={'1px'}
+          borderColor={'white'}
+          borderRadius={'10px'}
+          overflow={'hidden'}
+          className="cont"
+        >
+          <Box width={'100%'} h={'75%'} bg={'red'} overflow={'hidden'}>
+            <Image
+              src="./images/project.png"
+              height={'100%'}
+              objectFit={'cover'}
+              className="scale"
+            ></Image>
+          </Box>
+          <Box height={'100%'} width={'100%'}></Box>
+        </Box>
+        <Box boxSize={300} bg={'white'}></Box>
+        <Box boxSize={300} bg={'white'}></Box>
+        <Box boxSize={300} bg={'white'}></Box>
+      </Flex>
     </Container>
   );
 };
