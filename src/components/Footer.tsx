3f57b05147879e641bd48cbea06e17d12d5350db
redMulla <ivassillycharite@gmail.com>
2024-10-05 22:47:20 +0200

    formspree msg sent add

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -37,7 +37,7 @@ const ContactForm = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('https://formspree.io/f/YOUR_FORM_ID', {
+      const response = await fetch('https://formspree.io/f/xyzygjep', {
         method: 'POST',
         headers: {
           Accept: 'application/json',
